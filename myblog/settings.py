a9abedb8671dc2a3d9d519621187b402425ba664
harikrishnan51688 <harikrishnan51688@gmail.com>
2022-07-24 09:01:09 +0530

    img fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -166,7 +166,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'AYdPsWELWJJw2EsUTmzjaJbFQ78'      #os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Default primary key field type
