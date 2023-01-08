047d4b99ea82fd48bc076fb6f01afa77e8cb1df8
Hari <harikrishnan51688@gmail.com>
2023-01-08 19:30:22 +0530

    updated settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(tnyubck6=ely9)u3$qxu-1pk$#%(%s%_0^&b88sx(jqv4l!v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','web-production-b229.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://web-production-b229.up.railway.app'
