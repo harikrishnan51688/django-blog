50c8308f5d5b67c680f7a7b327105632e7477d34
Hari Krishnan <44374100+harikrishnan51688@users.noreply.github.com>
2023-01-08 10:10:28 +0530

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d(tnyubck6=ely9)u3$qxu-1pk$#%(%s%_0^&b88sx(jqv4l!v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-simpleblog.herokuapp.com', 'web-production-d95f.up.railway.app']
 
